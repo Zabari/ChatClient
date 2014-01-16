155e45ae1dadd149806651e58d184f71f17d7fbd
Zabari <zabari.daniel@Gmail.com>
2014-01-15 22:15:04 -0500

    first commit

---
+++ ChatGUI.java
@@ -0,0 +1,8 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+public class ChatGUI extends JFrame{
