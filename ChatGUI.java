787f201d905b0d0d54041f94b1ffdbdaf7b71728
Zabari <Zabari.daniel@Gmail.com>
2014-01-16 00:08:28 -0500

    first GUI

--- ChatGUI.java
+++ ChatGUI.java
@@ -5,4 +5,27 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
-public class ChatGUI extends JFrame{
+public class ChatGUI extends JFrame implements ActionListener{
+    static JFrame frame = new JFrame();
+    static JButton send = new JButton("Send");
+    public void actionPerformed(ActionEvent event){
+	send.setText("Sent!");
+    }
+    public void go(){
+	send.addActionListener(this);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setSize(300, 300);
+	frame.setLocationRelativeTo( null );
+	frame.getContentPane().add(send);
+    }
+    public static void main (String [] args){
+	ChatGUI try1= new ChatGUI();
+	try1.go();
+
+
+
+
+	frame.setVisible(true);
+
+    }
+}
