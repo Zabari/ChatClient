65f7be1a5eca65e235f867f2f26de802a7ed6c54
Philipp Steinmann <steinmann.philipp@yahoo.com>
2014-01-20 13:31:59 -0500

    Make indentation work for vim

--- ChatGUI.java
+++ ChatGUI.java
@@ -7,20 +7,15 @@ import java.io.IOException;
 import java.io.File;
 import org.jdesktop.xswingx.*;
 
-/*
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;*/
 import javax.swing.*;
 import java.awt.GridLayout;
-public class ChatGUI extends JFrame{
+public class ChatGUI extends JFrame {
 
-    GridLayout g = new GridLayout(4,4);
+    GridLayout g = new GridLayout(3,4);
     JFrame frame = new JFrame();
+
     JTextArea display = new JTextArea();
     JButton send = new JButton("Send");
-    JButton log = new JButton("See your chat so far!");
     JTextArea writeHere = new JTextArea();
 
     String message;
@@ -28,79 +23,48 @@ public class ChatGUI extends JFrame{
   
     /* Session chatSession;
        public ChatGUI(Session x){
-       chatSession=x;
-       make();
+           chatSession=x;
+           make();
        }
     */
 
     public class SentButtonListener implements ActionListener{
-	public void actionPerformed(ActionEvent event){
-	    message =  writeHere.getText();
-	    writeHere.setText("");
-	    /*
-	    try(PrintWriter chatLog = new PrintWriter(new BufferedWriter(new FileWriter("ChatLog.txt", true)))) {
-		    chatLog.println("You: "+message);
-		}
-	    catch (IOException ex)  {
-		System.out.println("Could not open file.  Try running as administrator or root.");
-	    } */
-	    if (message.trim().length()>0)
-		display.append("You: "+message.replaceFirst("\\s+$", "")+"\n");
-	}
+        public void actionPerformed(ActionEvent event){
+            message =  writeHere.getText();
+            writeHere.setText("");
+            if (message.trim().length() > 0)
+                display.append("You: "+ message.replaceFirst("\\s+$", "") + "\n");
+        }
     }
+
     public void make(){
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setSize(500, 500);
-	frame.setLocationRelativeTo( null );
-	frame.setLayout(g);
-	frame.getContentPane().add(new JScrollPane(display));
-        display.setLineWrap(true);
-        display.setWrapStyleWord(true);
-	display.setEditable(false);
-	frame.getContentPane().add(new JScrollPane(writeHere));
-        writeHere.setLineWrap(true);
-        writeHere.setWrapStyleWord(true);
-	frame.getContentPane().add(send);
-	PromptSupport.setPrompt("Type Here!",writeHere);
-	//	frame.getContentPane().add(log);
-	/*	log.addActionListener(new ActionListener()
-	    {
-		public void actionPerformed(ActionEvent e){
-		    File cLog=new File("Chatlog.txt"); 
-		    try{
-		    java.awt.Desktop.getDesktop().edit(cLog);
-		    }
-		    catch (IOException ex){
-		    }
-		}
-		}); */
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setLocationRelativeTo( null );
+        frame.setLayout(g);
 
-	send.addActionListener(new SentButtonListener());
-	/*	writeHere.addActionListener (new ActionListener()
-	    {
-		public void actionPerformed(ActionEvent event){
-		    message =  writeHere.getText();
-		    writeHere.setText("");
-		    display.setText("You: "+message);
-		    try(PrintWriter chatLog = new PrintWriter(new BufferedWriter(new FileWriter("ChatLog.txt", true)))) {
-			    chatLog.println("You: "+message);
-			}
-		    catch (IOException ex)  {
-			System.out.println("Could not open file.  Try running as administrator or root.");
-		    }
-		}
-		}); */
-	frame.setVisible(true);
+        frame.getContentPane().add(new JScrollPane(display));
+            display.setLineWrap(true);
+            display.setWrapStyleWord(true);
+        display.setEditable(false);
+        
+        frame.getContentPane().add(new JScrollPane(writeHere));
+            writeHere.setLineWrap(true);
+            writeHere.setWrapStyleWord(true);
+
+        frame.getContentPane().add(send);
+        PromptSupport.setPrompt("Type Here!",writeHere);
+        send.addActionListener(new SentButtonListener());
+
+        frame.setVisible(true);
     }
+
     public String getMessage(){
-	return message;
+        return message;
     }
-    public static void main (String [] args){
-	ChatGUI gui= new ChatGUI();
-	gui.make();
 
-
-
-
+    public static void main (String[] args){
+        ChatGUI gui= new ChatGUI();
+        gui.make();
     }
 }
